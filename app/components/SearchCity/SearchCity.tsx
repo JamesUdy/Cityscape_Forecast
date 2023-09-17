5110096bc856ec09374cca78a93b852a43b20c16
JamesUdy <udayamvad@gmail.com>
2023-09-17 11:25:19 +0530

    Refactor: Changed shadow sizing in searchCity section

--- app/components/SearchCity/SearchCity.tsx
+++ app/components/SearchCity/SearchCity.tsx
@@ -7,7 +7,7 @@ import { Search } from '@/app/assets';
 const SearchCity = () => {
   return (
     <>
-      <form className='bg-white hover:shadow-lg ease-in duration-200 hover:shadow-[#adb5bd] flex w-2/5 justify-between px-2 py-2 gap-2 mx-auto rounded-lg group'>
+      <form className='bg-white hover:shadow-xl ease-in duration-200 hover:shadow-[#adb5bd] flex w-2/5 justify-between px-2 py-2 gap-2 mx-auto rounded-lg group'>
         <input className='w-full mr-2 outline-none tracking-wider font-medium text-[#001233] group-hover:text-[#012a4a]' type="text" placeholder='🏙️ Search City...' />
         <Search />
       </form>
