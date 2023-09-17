a690d9f54e9f7043137178c3459c1ecbdce36072
JamesUdy <udayamvad@gmail.com>
2023-09-17 10:04:41 +0530

    Feat: Added Input Field for search city names

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -1,5 +1,7 @@
 import Logo from "./svg/Logo";
+import Search from "./svg/Search";
 
 export {
     Logo,
+    Search,
 };
--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -5,7 +5,7 @@ import SearchCity from '../SearchCity/SearchCity';
 
 const Home = () => {
   return (
-    <main className="bg-color h-screen px-10 py-6 flex flex-col space-y-4">
+    <main className="bg-color h-screen px-10 py-6 flex flex-col space-y-8">
       <Logo />
       <SearchCity />
     </main>
