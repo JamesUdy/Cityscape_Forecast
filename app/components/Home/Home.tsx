aedc797c5abe8c72e3cde3b3ccd0c5fda2125a7d
JamesUdy <udayamvad@gmail.com>
2023-09-17 07:34:29 +0530

    Ui: Styled Website Logo

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -5,7 +5,7 @@ import SearchCity from '../SearchCity/SearchCity';
 
 const Home = () => {
   return (
-    <main className="bg-color h-screen">
+    <main className="bg-color h-screen px-10 py-6 flex flex-col space-y-4">
       <Logo />
       <SearchCity />
     </main>
--- app/components/WebLogo/WebLogo.tsx
+++ app/components/WebLogo/WebLogo.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Logo } from '@/app/assets';
 
+import './WebLogo.css';
+
 const WebLogo = () => {
   return (
-    <section className='flex items-center space-x-2'>
+    <section className='flex items-center group space-x-2'>
         <Logo />
-        <span>Cityscape Forecast</span>
+        <span className='text-4xl text-[#001233] group-hover:text-[#012a4a] ease-in duration-300'>Cityscape Forecast</span>
     </section>
   )
 };
