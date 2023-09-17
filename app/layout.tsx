f06f72f5e16ce32f6d545b1f9be988e6d98a8d54
JamesUdy <udayamvad@gmail.com>
2023-09-17 07:49:05 +0530

    [wip] Feat: Adding favicon to the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Cityscape Forecast | Urban Weather Unveiled',
   description: "Navigating the Weather of Urban Life. Discover your city's ever-changing atmospheric canvas with accurate forecasts and climate insights. Get in tune with your urban surroundings, and stay one step ahead of the elements with CityscapeForecast.",
-}
+};
 
 export default function RootLayout({
   children,
@@ -19,4 +19,4 @@ export default function RootLayout({
       <body className={inter.className}>{children}</body>
     </html>
   )
-}
+};
