6500024c6c78fd2f11057c4eccaaadb1379dfcee
JamesUdy <udayamvad@gmail.com>
2023-09-17 10:09:23 +0530

    Feat: Added functionalities to the website Logo

--- app/components/WebLogo/WebLogo.tsx
+++ app/components/WebLogo/WebLogo.tsx
@@ -4,12 +4,16 @@ import { Logo } from '@/app/assets';
 
 import './WebLogo.css';
 
+import Link from 'next/link';
+
 const WebLogo = () => {
   return (
-    <section className='flex items-center group space-x-2'>
-        <Logo />
-        <span className='text-4xl text-[#001233] group-hover:text-[#012a4a] ease-in duration-300'>Cityscape Forecast</span>
-    </section>
+    <>
+        <Link href='#' className='flex items-center group space-x-2'>
+          <Logo />
+          <span className='text-4xl text-[#001233] group-hover:text-[#012a4a] ease-in duration-300'>Cityscape Forecast</span>
+        </Link>
+    </>
   )
 };
 
