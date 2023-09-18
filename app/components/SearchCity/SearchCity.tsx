adb9970102bcbda4fd5d97c8e349a4374600c29b
JamesUdy <udayamvad@gmail.com>
2023-09-18 22:57:49 +0530

    Feat: Updated form id and name in searchCity section

--- app/components/SearchCity/SearchCity.tsx
+++ app/components/SearchCity/SearchCity.tsx
@@ -12,7 +12,7 @@ interface InputProps {
 const SearchCity = ({handleSearch, setLocationData}: InputProps) => {
   return (
     <>
-      <form className='bg-white shadow-xl ease-in duration-200 hover:ring-2 hover:ring-[#adb5bd86] shadow-[#adb5bd] hover:shadow-[#001f3f6b] flex w-2/5 justify-between px-2 py-2 gap-2 mx-auto rounded-lg group'>
+      <form id='cityName' name='cityName' className='bg-white shadow-xl ease-in duration-200 hover:ring-2 hover:ring-[#adb5bd86] shadow-[#adb5bd] hover:shadow-[#001f3f6b] flex w-2/5 justify-between px-2 py-2 gap-2 mx-auto rounded-lg group'>
         <input className='w-full mr-2 outline-none tracking-wider font-medium text-[#001233] group-hover:text-[#012a4a]' type="text" placeholder='🏙️ Search City...' onKeyDown={handleSearch} onChange={(e) => setLocationData(e.target.value)} />
         <Search />
       </form>
