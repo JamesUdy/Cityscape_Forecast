5215a69a6397b8928f0db3dd9854ba1bf9f8c873
JamesUdy <udayamvad@gmail.com>
2023-09-20 22:27:32 +0530

    Feat: Added meta data for twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
+      <head>        
         <meta property="og:url" content="https://cityscape-forecast.vercel.app/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Cityscape Forecast | Urban Weather Unveiled" />
@@ -24,6 +24,14 @@ export default function RootLayout({
         <meta property="og:image" content="https://i.imgur.com/qqvNslC.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:domain" content="cityscape-forecast.vercel.app"/>
+        <meta property="twitter:url" content="https://cityscape-forecast.vercel.app/"/>
+        <meta name="twitter:title" content="Cityscape Forecast | Urban Weather Unveiled"/>
+        <meta name="twitter:description" content="Navigating the Weather of Urban Life. Discover your city's ever-changing atmospheric canvas with accurate forecasts and climate insights. Get in tune with your urban surroundings, and stay one step ahead of the elements with CityscapeForecast."/>
+        <meta name="twitter:image" content="https://i.imgur.com/qqvNslC.png"/>
+        <meta property="twitter:image:width" content="1200" />
+        <meta property="twitter:image:height" content="630" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
