e08102733c86c28a639f546b274ba5a14d0c0e55
JamesUdy <udayamvad@gmail.com>
2023-09-21 10:35:15 +0530

    Feat: Made API renders at server side than it client side

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,30 +1,32 @@
+"use client";
+
 import React, { useState } from 'react';
 
 import Logo from '../WebLogo/WebLogo';
 import SearchCity from '../SearchCity/SearchCity';
 
 import WeatherContent from '../WeatherContent/WeatherContent';
-import AirQualityIndex from '../AirQualityIndex/AirQualityIndex';
 
-const Home = () => {
+interface HomeProps {
+  weatherURLKey: string;
+  airQualityURLKey: string;
+}
+
+const Home: React.FC<HomeProps> = ({ weatherURLKey, airQualityURLKey }) => {
   const [weatherData, setWeatherData] = useState({});
   const [AQIndex, setAQIndex] = useState<number | null>(null);
   const [locationData, setLocationData] = useState("");
   const [error, setError] = useState("");
 
-  // const weatherAPIKey = process.env.REACT_APP_API_KEY;
+  const weatherAPI = `https://api.weatherapi.com/v1/forecast.json?key=${weatherURLKey}&q=${locationData}&days=3&aqi=yes&alerts=yes`;
 
-  // console.log(process.env.REACT_APP_API_KEY);
-
-  const weatherURL = `https://api.weatherapi.com/v1/forecast.json?key=472f53b61fb9488594d34124231609&q=${locationData}&days=3&aqi=yes&alerts=yes`;
-
-  const airQualityURL = `https://api.waqi.info/feed/${locationData}/?token=1d3c6abb2c9bc75b89eeb13dad7deed605271097`;
+  const airQualityURL = `https://api.waqi.info/feed/${locationData}/?token=${airQualityURLKey}`;
 
   const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if(e.key === "Enter") {
       e.preventDefault()
         try {
-          const res1 = await fetch(weatherURL);
+          const res1 = await fetch(weatherAPI);
           const res2 = await fetch(airQualityURL);
           if(!res1.ok) {
             throw new Error()
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-"use client";
-
 import Home from "./components/Home/Home";
 
 const page = () => {
+  const weatherAPIKey = process.env.WEATHER_API_KEY;
+  const airQualityAPIKey = process.env.AIR_QUALITY_API_KEY;
+  
+  const airQualityURLKey = `${airQualityAPIKey}`;
+  const weatherURLKey = `${weatherAPIKey}`;
   return (
-    <Home />
+    <Home weatherURLKey={weatherURLKey} airQualityURLKey={airQualityURLKey} />
   )
 };
 
