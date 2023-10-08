2859f0779840079ec6ce0b5955606014e4e7cd20
JamesUdy <udayamvad@gmail.com>
2023-10-08 18:14:32 +0530

    fix: made process env works

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Home from "./components/Home/Home";
 
 const page = () => {
-  const weatherAPIKey = String(process.env.REACT_APP_WEATHER_API_KEY);
-  const airQualityAPIKey = String(process.env.REACT_APP_AIR_QUALITY_API_KEY);
+  const weatherAPIKey = String(process.env.WEATHER_API_KEY);
+  const airQualityAPIKey = String(process.env.AIR_QUALITY_API_KEY);
 
   return (
     <Home weatherAPIKey={weatherAPIKey} airQualityAPIKey={airQualityAPIKey} />
